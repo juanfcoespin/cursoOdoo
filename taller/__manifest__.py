317fba9ef0908aa3ed707c2b6a42cd64f962d22f
juanfcoespin <juan.espin@yahoo.com>
2020-08-01 09:55:39 -0500

    PrimerModulo Finalizado

--- taller/__manifest__.py
+++ taller/__manifest__.py
@@ -1,22 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "taller",
+    'name': "Gestion Taller",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Gestion de Vehiculos y Reparaciones
+        """,
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'author': "TechTools",
+    'website': "http://www.techtoolsec.com",
+    'category': 'Gestion Vehiculo',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,12 +20,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        "security/ir.model.access.csv",
+        "views/vehiculo_view.xml",
     ],
 }
+
